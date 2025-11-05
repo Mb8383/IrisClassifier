0eeeba34ab0a144272acd30c3c5b4bc28dc66c1b
Mohamad bayat <146379941+Mb8383@users.noreply.github.com>
2025-11-06 02:06:52 +0330

    Add tage in data

--- IrisClassifier/IrisClassifier.py
+++ IrisClassifier/IrisClassifier.py
@@ -4,5 +4,7 @@ from sklearn.datasets import load_iris
 
 iris=load_iris();
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df["species"]=iris.target
-print(df.head)
+
+df["target"]=iris.target
+df["species"]=df["target"].apply(lambda x:iris.target_names[x])
+print(df.head(100))
