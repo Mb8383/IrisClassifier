1120da63f6c459ab01b6b2ddf9943f22bc867ab2
Mohamad bayat <146379941+Mb8383@users.noreply.github.com>
2025-11-06 01:50:52 +0330

    Load Data

--- IrisClassifier/IrisClassifier.py
+++ IrisClassifier/IrisClassifier.py
@@ -1,1 +1,8 @@
+import tensorflow as tf
+import pandas as pd
+from sklearn.datasets import load_iris
 
+iris=load_iris();
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df["species"]=iris.target
+print(df.head)
